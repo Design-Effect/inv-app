d4a2c53bb15b1c8d7831d54b1e6a16664e9f713c
ferreiradasilvaricardo1070-blip <ferreiradasilvaricardo1070@gmail.com>
2026-09-05 21:02:29 +0200

    Beta : marques reelles au lieu des contenances, et retrait d'une photo de mauvaise marque

--- beta/sw.js
+++ beta/sw.js
@@ -1,7 +1,7 @@
 // Monter ce numero a CHAQUE modification d'index.html/app.js/data.js : sinon
 // sw.js ne change pas, le navigateur ne redetecte aucune mise a jour, et le
 // telephone sert l'ancienne version indefiniment.
-var CACHE = 'hs-stock-beta-v1';
+var CACHE = 'hs-stock-beta-v2';
 var ASSETS = ['./', './index.html', './app.js', './data.js', './photos.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 // Les images produit (img/*.webp) ne sont PAS prechargees : 205 fichiers, 1,8 Mo.
 // Le gestionnaire fetch ci-dessous les met en cache au fur et a mesure qu'elles
