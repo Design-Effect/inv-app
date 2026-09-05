3acb647dea57c5023b34fd8723edd4de394010ab
ferreiradasilvaricardo1070-blip <ferreiradasilvaricardo1070@gmail.com>
2026-09-05 20:51:06 +0200

    Retour a la version stable en production, nouvelle version isolee dans beta/

--- beta/sw.js
+++ beta/sw.js
@@ -1,8 +1,11 @@
 // Monter ce numero a CHAQUE modification d'index.html/app.js/data.js : sinon
 // sw.js ne change pas, le navigateur ne redetecte aucune mise a jour, et le
 // telephone sert l'ancienne version indefiniment.
-var CACHE = 'hs-stock-v1.15';
-var ASSETS = ['./', './index.html', './app.js', './data.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+var CACHE = 'hs-stock-beta-v1';
+var ASSETS = ['./', './index.html', './app.js', './data.js', './photos.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+// Les images produit (img/*.webp) ne sont PAS prechargees : 205 fichiers, 1,8 Mo.
+// Le gestionnaire fetch ci-dessous les met en cache au fur et a mesure qu'elles
+// s'affichent, donc elles restent disponibles hors-ligne une fois vues.
 
 self.addEventListener('install', function (e) {
   e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ASSETS); }).then(function(){ return self.skipWaiting(); }));
@@ -10,7 +13,9 @@ self.addEventListener('install', function (e) {
 
 self.addEventListener('activate', function (e) {
   e.waitUntil(caches.keys().then(function (keys) {
-    return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+    // BETA : ne supprime QUE ses propres caches, sinon elle effacerait celui
+    // de l'app en service (et reciproquement) sur un telephone qui a les deux.
+    return Promise.all(keys.filter(function (k) { return k !== CACHE && k.indexOf('hs-stock-beta-') === 0; }).map(function (k) { return caches.delete(k); }));
   }).then(function(){ return self.clients.claim(); }));
 });
 
